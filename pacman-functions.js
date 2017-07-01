720fd8bce568586f47f6b41ff354cfff3b0d190d
Ronen Annason <ronenannason83@gmail.com>
2017-07-01 19:01:57 -0400

    Intro to JS complete

---
+++ pacman-functions.js
@@ -0,0 +1,13 @@
+//named function
+
+function eatDot() {
+  console.log("Munch!");
+}
+
+
+//Anonymous function
+//here we assign function to a variable instead of giving it a name directly
+
+var eatDot = function() {
+  console.log("Munch!");
+}
